cf701d50f181e8d4261d2fa06d71531f7648447c
Belyse <iintwazabelyse@gmail.com>
2025-06-27 07:45:14 +0200

    new changes

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        if not row:  # if the row is empty, skip printing anything
-            continue
-        for i, num in enumerate(row):
+        for i in range(len(row)):
             if i != len(row) - 1:
-                print("{:d}".format(num), end=" ")
+                print("{:d}".format(row[i]), end=" ")
             else:
-                print("{:d}".format(num))
+                print("{:d}".format(row[i]), end="")
+        print()
