3d712976d648bdf5566988e593943c9d8f12805c
Belyse <iintwazabelyse@gmail.com>
2025-06-27 07:41:47 +0200

    new changes

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,9 +2,7 @@
 def new_in_list(my_list, idx, element):
     # Create a copy of the original list
     new_list = my_list[:]
-    
     # Check if index is valid
     if 0 <= idx < len(new_list):
         new_list[idx] = element
-    
     return new_list
