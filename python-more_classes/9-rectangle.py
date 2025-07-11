8ba8253364f8004936f6282b7dcfc186d8608f7f
Belyse <iintwazabelyse@gmail.com>
2025-07-11 13:50:53 +0200

    add python exceptions file

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -53,7 +53,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-    
+
     def __str__(self):
         """Return string representation using print_symbol."""
         if self.__width == 0 or self.__height == 0:
