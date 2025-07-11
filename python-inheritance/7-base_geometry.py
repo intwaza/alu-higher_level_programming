bc9bc8f4497ef9149700e36476ce3b18f261a01e
Belyse <iintwazabelyse@gmail.com>
2025-07-11 17:22:11 +0200

    add python everything is object

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -23,10 +23,11 @@ class BaseGeometry:
             value (int): The value to validate.
 
         Raises:
-            TypeError: If value is not an integer.
+            TypeError: If value is not an integer or is a boolean.
             ValueError: If value is less than or equal to 0.
         """
-        if not isinstance(value, int):
+        if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
