22198bee671277d214049ad3abcd84c267c6162f
Belyse <iintwazabelyse@gmail.com>
2025-07-24 17:04:31 +0200

    add python network 0

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a GET request and displays body only for 200 status code responses
-[ "$(curl -s -w "%{http_code}" -o /tmp/body "$1")" = "200" ] && cat /tmp/body; rm -f /tmp/body
+[ "$(curl -s -L -w "%{http_code}" -o /tmp/body "$1")" = "200" ] && cat /tmp/body; rm -f /tmp/body
