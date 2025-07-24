c8fdfe6e2b1b6c5590f5ec8796eb4b9200994cb3
Belyse <iintwazabelyse@gmail.com>
2025-07-24 16:59:32 +0200

    add python network 0

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-# Script that sends a GET request
-
-# Create temporary file to store response body
-temp_file=$(mktemp)
-
-# Send request, save body to temp file, and get status code
-status_code=$(curl -s -w "%{http_code}" -o "$temp_file" "$1")
-
-# Only display body if status code is 200
-if [ "$status_code" -eq 200 ]; then
-    cat "$temp_file"
-fi
-
-# Clean up temporary file
-rm "$temp_file"
+# Script that sends a GET request and displays body only for 200 status code responses
+[ "$(curl -s -w "%{http_code}" -o /tmp/body "$1")" = "200" ] && cat /tmp/body; rm -f /tmp/body
