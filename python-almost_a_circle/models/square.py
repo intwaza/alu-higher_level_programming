814dd9078b935d66f820eee9c571cf1c7dce62f1
Belyse <iintwazabelyse@gmail.com>
2025-07-30 12:17:25 +0200

    new changes

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -53,7 +53,7 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
-    
+
     def to_dictionary(self):
         """
         Return the dictionary representation of the Square
