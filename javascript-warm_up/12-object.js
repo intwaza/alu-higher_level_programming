d97f71129473dd287be973a16ab558df9e37db3a
bintwaza <b.intwaza@alustudent.com>
2025-09-30 15:34:31 +0200

    javascript-warm_up

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -3,10 +3,9 @@ const myObject = {
     type: 'object',
     value: 12
   }
-  console.log(myObject)
-  
+console.log(myObject)
+
   /* Update the value property */
-  myObject.value = 89
-  
-  console.log(myObject)
-  
+myObject.value = 89
+
+console.log(myObject)
